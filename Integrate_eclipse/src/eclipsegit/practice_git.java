696c3081e9187f16bae7e096168c5d20e4192c8a
laksh <laksh@LAPTOP-PVI4QB05>
2024-02-06 13:27:37 +0530

    EclipseIntegration

---
+++ Integrate_eclipse/src/eclipsegit/practice_git.java
@@ -0,0 +1,18 @@
+package eclipsegit;
+
+public class practice_git
+{
+public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something");
+		}
+		else
+		{
+			System.out.println("go to sleep");
+		}
+}
+}
---
+++ Integrate_eclipse/src/module-info.java
@@ -0,0 +1,2 @@
+module Integrate_eclipse {
+}
